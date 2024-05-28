5af884ec3fc3c93744c10b0e31d4ab451e6ac472
carlosjnr0 <105026245+carlosjnr0@users.noreply.github.com>
2024-05-28 16:22:31 +0000

    upt

--- Portfolio-site-2/js/script.js
+++ Portfolio-site-2/js/script.js
@@ -3,13 +3,17 @@ let nav = document.getElementById("mnav")
 let loading = document.getElementById("lding")
 
 function tg(){
-    button.classList.toggle("aactive")
+    if (button.click){
+         button.classList.toggle("aactive")
+    }
+   
 }
 
 document.addEventListener("click", (event) => {
         if (event.target.id !== "mnav" && event.target.id !== "anim") {
             button.classList.remove("aactive");
         }
+       
  });
  const delay = ms => new Promise(res => setTimeout(res, ms));
 let bbody=document.getElementById('body');
