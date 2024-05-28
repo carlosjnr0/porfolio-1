51bd19d2fc99d6b2ecf92ad5900c99343f357205
carlosjnr0 <105026245+carlosjnr0@users.noreply.github.com>
2024-05-28 13:46:44 +0000

    upd

--- Portfolio-site-2/js/script.js
+++ Portfolio-site-2/js/script.js
@@ -1,18 +1,24 @@
 let button = document.getElementById("anim")
 let nav = document.getElementById("mnav")
+let loading = document.getElementById("lding")
 
 function tg(){
     button.classList.toggle("aactive")
 }
 
-let bbody=document.getElementById('body');
-        function showpage(){
-            bbody.classList.add('show')
-        }
-
-
- document.addEventListener("click", (event) => {
+document.addEventListener("click", (event) => {
         if (event.target.id !== "mnav" && event.target.id !== "anim") {
             button.classList.remove("aactive");
         }
  });
+ const delay = ms => new Promise(res => setTimeout(res, ms));
+let bbody=document.getElementById('body');
+        async function showpage(){
+            bbody.classList.add('show')
+            await delay(3000);
+            loading.classList.add('lding-hide')
+            
+        }
+
+
+ 
